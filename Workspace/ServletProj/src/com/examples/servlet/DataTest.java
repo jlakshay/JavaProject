7fa8e0c8c2d1abf4955e9e3677a1c172d1dbb01b
jlakshay <j_lakshay@yahoo.in>
2017-07-13 22:59:30 -0700

    This is my Servlet

---
+++ Workspace/ServletProj/src/com/examples/servlet/DataTest.java
@@ -0,0 +1,16 @@
+package com.examples.servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataTest {
+
+	DisplayServ obj= new DisplayServ();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
